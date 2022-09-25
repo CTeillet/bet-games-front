077443f13d161096afc3f80f2e23542876474f2b
Teillet Corentin <teillet.corentin@neuf.fr>
2022-09-25 11:36:53 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import MenuBar from "./MenuBar";
+import ListMatch from "./ListMatch";
+
+function App() {
+    return (
+        <div>
+            <header>
+                <MenuBar></MenuBar>
+            </header>
+            <ListMatch></ListMatch>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/ListMatch.tsx
@@ -0,0 +1,73 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Typography from "@mui/material/Typography";
+
+export default function ListMatch() {
+    return (
+        <Box sx={{width: '100%', maxWidth: 1000, bgcolor: 'background.paper'}}>
+            <Typography variant="h1" gutterBottom>
+                Matchs à venir
+            </Typography>
+            <nav aria-label="main mailbox folders">
+                <List>
+                    <ListItem>
+                        <ListItemText primary="League of Legends"/>
+
+                    </ListItem>
+                    <ListItem>
+
+                        <List>
+                            <ListItem>
+                                <ListItemText primary="Imbox"/>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText primary="Drafts"/>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText primary="Outbox"/>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText primary="Outbox"/>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText primary="Outbox"/>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText primary="Outbox"/>
+                            </ListItem>
+                        </List>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary="CSGO"/>
+                    </ListItem>
+                    <ListItem>
+                        <List>
+                            <ListItem>
+                                <ListItemText primary="Imbox"/>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText primary="Drafts"/>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText primary="Outbox"/>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText primary="Outbox"/>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText primary="Outbox"/>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText primary="Outbox"/>
+                            </ListItem>
+                        </List>
+                    </ListItem>
+
+                </List>
+            </nav>
+        </Box>
+    );
+}
